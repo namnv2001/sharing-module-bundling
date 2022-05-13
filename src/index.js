2893732925196dbc2f7d08bbf2368f60dbb8a0d2
namnv2001 <namnv.forwork@gmail.com>
2022-05-13 10:58:52 +0700

    module bundling

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Module bundling</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
